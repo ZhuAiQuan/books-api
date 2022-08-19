c3af537574e722093bc7c2d35afce98036ba264a
zhuaq <zhuaq@7000mall.com>
2022-08-19 11:07:37 +0800

    暂存

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ const babayu = require('./babayu');
 
 routes.use('/babayu', babayu.routes(), babayu.allowedMethods());
 
-routes.redirect('/', '/babayu')
+// routes.redirect('/', '/babayu')
+routes.get('/', async ctx => {
+  ctx.body = `welcome...`
+})
 
 module.exports = routes
